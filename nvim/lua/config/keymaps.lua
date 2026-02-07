f3c56b4845182eec9d148aaa0b59527db4b8daa8
Abdiel Mendez <124847316+abdiel-sys@users.noreply.github.com>
2026-02-07 15:09:58 -0600

    changes on hyprland files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,9 @@
-local map = function (mode, k, c, desc) 
-    vim.keymap.set(mode, k, c, { noremap = true, silent = true, desc = desc })
+local opts = {
+    noremap = true,
+    silent = true
+}
+local map = function (mode, k, c, opt) 
+    vim.keymap.set(mode, k, c, opt)
 end
 vim.g.mapleader = ' '
 -----------------
@@ -7,25 +11,31 @@ vim.g.mapleader = ' '
 -----------------
 
 -- Window navigation
-map('n', '<C-h>', '<C-w>h')
-map('n', '<C-j>', '<C-w>j')
-map('n', '<C-k>', '<C-w>k')
-map('n', '<C-l>', '<C-w>l')
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
 
 -- Resize with arrows
-map('n', '<C-Up>', ':resize -2<CR>')
-map('n', '<C-Down>', ':resize +2<CR>')
-map('n', '<C-Left>', ':vertical resize -2<CR>')
-map('n', '<C-Right>', ':vertical resize +2<CR>')
+map('n', '<C-Up>', ':resize -2<CR>', opts)
+map('n', '<C-Down>', ':resize +2<CR>', opts)
+map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
+map('n', '<leader>w', ':w<CR>', opts)
 -----------------
 -- Visual mode --
 -----------------
 
-map('v', '<', '<gv')
-map('v', '>', '>gv')
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
 
 -- mini files
-map('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>', "Open MiniFiles")
+map('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>', {desc = "Open Files"})
+
+-- Bufferline
+map('n', '<leader>l', ':BufferLineCycleNext<CR>', {desc = "Go to next buffer"})
+map('n', '<leader>h', ':BufferLineCyclePrev<CR>', {desc = "Go to previous buffer"})
+map('n', '<leader>q', ':BufferLinePickClose<CR>', {desc = "Go to previous buffer"})
 
 
